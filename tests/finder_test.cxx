42b8499baa354b83862e02f531b168f439443ecc
Dimitrij Mijoski <dmjpp@hotmail.com>
2017-12-18 00:36:04 +0100

    Cleanup in licenses

--- tests/finder_test.cxx
+++ tests/finder_test.cxx
@@ -1,19 +1,19 @@
 /* Copyright 2017 Sander van Geloven
  *
- * This file is part of Hunspell.
+ * This file is part of Nuspell.
  *
- * Hunspell is free software: you can redistribute it and/or modify
+ * Nuspell is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Hunspell is distributed in the hope that it will be useful,
+ * Nuspell is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Hunspell.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Nuspell.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "catch.hpp"
