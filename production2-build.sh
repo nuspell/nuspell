d7b7c17dcd72b28ddb5f82a1d630804522658403
PanderMusubi <pander@users.sourceforge.net>
2018-05-25 16:20:00 +0200

    refactoring

--- production2-build.sh
+++
@@ -1,5 +0,0 @@
-make CXXFLAGS='-g -O2 -fno-omit-frame-pointer'
-if [ $? -ne 0 ]; then
-	echo FAILED
-	exit $?
-fi
--- profiling-reconfigure-and-build.sh
+++ profiling-reconfigure-and-build.sh
@@ -5,3 +5,8 @@ else
 	echo FAILED
 	exit $?
 fi
+make CXXFLAGS='-g -O2 -fno-omit-frame-pointer'
+if [ $? -ne 0 ]; then
+	echo FAILED
+	exit $?
+fi
