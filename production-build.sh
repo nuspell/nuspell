b4971278cc5f7992c842133025a5d870a641ae26
Dimitrij Mijoski <dmjpp@hotmail.com>
2018-10-07 23:09:55 +0200

    Delete scripts, use ./configure --enable-debug
    or ./configure --enable-profile

--- production-build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-make -j
-if [ $? -ne 0 ]; then
-	echo 'ERROR: Failed to build nuspell for production'
-	exit 1
-fi
--- tests/segment.sh
+++
@@ -1,9 +0,0 @@
-set -ex
-../src/nuspell/nuspell -d v1cmdline/base segment.txt 2> /dev/null
-../src/nuspell/nuspell -S -d v1cmdline/base segment.txt 2> /dev/null
-../src/nuspell/nuspell -U -d v1cmdline/base segment.txt 2> /dev/null
-../src/nuspell/nuspell -S -U -d v1cmdline/base segment.txt 2> /dev/null
-../src/nuspell/nuspell -l -d v1cmdline/base segment.txt 2> /dev/null
-../src/nuspell/nuspell -G -d v1cmdline/base segment.txt 2> /dev/null
-../src/nuspell/nuspell -G -L -d v1cmdline/base segment.txt 2> /dev/null
-../src/nuspell/nuspell -l -L -d v1cmdline/base segment.txt 2> /dev/null
