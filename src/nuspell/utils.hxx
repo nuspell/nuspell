d39e2e130bf97d1ca1bd438c17086e12143ba5c2
Benny Siegert <bsiegert@gmail.com>
2020-02-02 12:29:31 +0000

    Do not try to use uselocale on NetBSD.
    NetBSD does not have a uselocale library call.

--- src/nuspell/utils.hxx
+++ src/nuspell/utils.hxx
@@ -125,7 +125,7 @@ class Encoding_Converter {
 };
 
 //#if _POSIX_VERSION >= 200809L
-#ifdef _POSIX_VERSION
+#if defined(_POSIX_VERSION) && !defined(__NetBSD__)
 class Setlocale_To_C_In_Scope {
 	locale_t old_loc = nullptr;
 
