209a68e0ad55a6e0e47ea4d71ceff627b94acc5b
Dimitrij Mijoski <dmjpp@hotmail.com>
2021-06-10 22:44:25 +0200

    Move the new class Suggester to separate files

--- src/nuspell/dictionary.hxx
+++ src/nuspell/dictionary.hxx
@@ -24,78 +24,10 @@
 #ifndef NUSPELL_DICTIONARY_HXX
 #define NUSPELL_DICTIONARY_HXX
 
-#include "checker.hxx"
+#include "suggester.hxx"
 
 namespace nuspell {
 inline namespace v5 {
-
-struct NUSPELL_EXPORT Suggester : public Checker {
-
-	enum High_Quality_Sugs : bool {
-		ALL_LOW_QUALITY_SUGS = false,
-		HAS_HIGH_QUALITY_SUGS = true
-	};
-
-	auto suggest_priv(std::string_view input_word, List_Strings& out) const
-	    -> void;
-
-	auto suggest_low(std::string& word, List_Strings& out) const
-	    -> High_Quality_Sugs;
-
-	auto add_sug_if_correct(std::string& word, List_Strings& out) const
-	    -> bool;
-
-	auto uppercase_suggest(const std::string& word, List_Strings& out) const
-	    -> void;
-
-	auto rep_suggest(std::string& word, List_Strings& out) const -> void;
-
-	auto try_rep_suggestion(std::string& word, List_Strings& out) const
-	    -> void;
-
-	auto max_attempts_for_long_alogs(std::string_view word) const -> size_t;
-
-	auto map_suggest(std::string& word, List_Strings& out) const -> void;
-
-	auto map_suggest(std::string& word, List_Strings& out, size_t i,
-	                 size_t& remaining_attempts) const -> void;
-
-	auto adjacent_swap_suggest(std::string& word, List_Strings& out) const
-	    -> void;
-
-	auto distant_swap_suggest(std::string& word, List_Strings& out) const
-	    -> void;
-
-	auto keyboard_suggest(std::string& word, List_Strings& out) const
-	    -> void;
-
-	auto extra_char_suggest(std::string& word, List_Strings& out) const
-	    -> void;
-
-	auto forgotten_char_suggest(std::string& word, List_Strings& out) const
-	    -> void;
-
-	auto move_char_suggest(std::string& word, List_Strings& out) const
-	    -> void;
-
-	auto bad_char_suggest(std::string& word, List_Strings& out) const
-	    -> void;
-
-	auto doubled_two_chars_suggest(std::string& word,
-	                               List_Strings& out) const -> void;
-
-	auto two_words_suggest(const std::string& word, List_Strings& out) const
-	    -> void;
-
-	auto ngram_suggest(const std::string& word_u8, List_Strings& out) const
-	    -> void;
-
-	auto expand_root_word_for_ngram(Word_List::const_reference root,
-	                                std::string_view wrong,
-	                                List_Strings& expanded_list,
-	                                std::vector<bool>& cross_affix) const
-	    -> void;
-};
 
 /**
  * @brief The only important public exception
@@ -121,6 +53,7 @@ class NUSPELL_EXPORT Dictionary : private Suggester {
 	auto suggest(std::string_view word, std::vector<std::string>& out) const
 	    -> void;
 };
+
 } // namespace v5
 } // namespace nuspell
 #endif // NUSPELL_DICTIONARY_HXX
